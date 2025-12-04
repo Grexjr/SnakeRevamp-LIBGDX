a70069ee746a9bcfd3429da91c1be5a6d5e8b165
grexjr <geerexed@gmail.com>
2025-12-03 23:02:15 -0500

    ENEMY
    Adds the enemy class which repeats a lot of code from the player class.
    
    Will include a sightline and search function alongside phases. For now, just getting the enemy placed and moving on screen is the immediate goal.

---
+++ core/src/main/java/com/badlogic/snakerevamp/obj/Enemy.java
@@ -0,0 +1,178 @@
+package com.badlogic.snakerevamp.obj;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.particles.values.RectangleSpawnShapeValue;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.ArrayList;
+
+public class Enemy {
+
+    private Texture enemyTexture;
+    private ArrayList<Sprite> bodySprites;
+    private ArrayList<Rectangle> bodyRectangles;
+    private ArrayList<Vector2> lastPositions;
+    // Sees in a straight line of x,y positions
+    // In the sight check, if a rectangle is found, stop the search and return the rectangle found
+    private ArrayList<Vector2> sight;
+
+    // Need phase for AI--later
+
+    public Enemy(int size){
+        // Build the body texture
+        Pixmap bodyMap = new Pixmap(1,1, Pixmap.Format.RGB888);
+        bodyMap.setColor(Color.BLUE);
+        bodyMap.fill();
+
+        // Set the texture
+        enemyTexture = new Texture(bodyMap);
+        bodyMap.dispose();
+
+        bodySprites = new ArrayList<>();
+        bodyRectangles = new ArrayList<>();
+        lastPositions = new ArrayList<>();
+        sight = new ArrayList<>();
+        // Add sprites and rectangles based on size of the enemy
+        for(int i = 0; i < size; i++){
+            Sprite segment = new Sprite(enemyTexture);
+            segment.setSize(1,1);
+            bodySprites.add(segment);
+            bodyRectangles.add(new Rectangle());
+        }
+    }
+
+    public Sprite getHeadSprite(){
+        return bodySprites.get(0);
+    }
+
+    public Rectangle getHeadRectangle(){return bodyRectangles.get(0);}
+
+    // Initializes the Enemy's body sprites and rectangles
+    public void initEnemy(float xPos, float yPos){
+        // Set position of snake to the values
+        bodySprites.get(0).setPosition(
+            xPos,
+            yPos
+        );
+        bodyRectangles.get(0).set(
+            xPos,
+            yPos,
+            bodySprites.get(0).getWidth(),
+            bodySprites.get(0).getHeight()
+        );
+
+        // Initialize body to offscreen at first
+        for (int i = 1; i < bodySprites.size(); i++) {
+            bodySprites.get(i).setPosition(
+                -999,
+                999
+            );
+        }
+        // Initialize rectangles of body to offscreen
+        for (int i = 1; i < bodyRectangles.size(); i++) {
+            bodyRectangles.get(i).set(
+                -999,
+                999,
+                bodySprites.get(i).getWidth(),
+                bodySprites.get(i).getHeight()
+            );
+        }
+    }
+
+    // Method for moving the snake properly
+    public void moveHead(float dirX, float dirY, float upperBoundX, float upperBoundY){
+        Sprite head = bodySprites.get(0);
+        Rectangle headRect = bodyRectangles.get(0);
+
+        // Clear and save all old positions before moving anything
+        lastPositions.clear();
+        for (Sprite s : bodySprites) {
+            lastPositions.add(new Vector2(s.getX(), s.getY()));
+        }
+
+        // Move head sprite and update rectangle to sprite location
+        head.setPosition(
+            head.getX() + dirX,
+            head.getY() + dirY
+        );
+        headRect.set(
+            head.getX(),
+            head.getY(),
+            head.getWidth(),
+            head.getHeight()
+        );
+
+        // Wrap around code
+        if (head.getX() > upperBoundX) {
+            head.setX(0);
+            headRect.setX(0);
+        }
+        if (head.getX() < 0) {
+            head.setX(upperBoundX);
+            headRect.setX(upperBoundX);
+        }
+        if (head.getY() > upperBoundY) {
+            head.setY(0);
+            headRect.setY(0);
+        }
+        if (head.getY() < 0) {
+            head.setY(upperBoundY);
+            headRect.setY(upperBoundY);
+        }
+    }
+
+    // Method for checking collision with body
+    public boolean runBodyCollisionCheck(){
+        Rectangle headRect = bodyRectangles.get(0);
+
+        for (int i = 1; i < bodyRectangles.size(); i++) {
+            if (headRect.overlaps(bodyRectangles.get(i))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Method for moving the rest of the body
+    public void moveBody(){
+        // Move the rest of the body and update rectangles
+        for (int i = bodySprites.size() - 1; i > 0; i--) {
+            bodySprites.get(i).setPosition(
+                lastPositions.get(i - 1).x,
+                lastPositions.get(i - 1).y
+            );
+            bodyRectangles.get(i).set(
+                lastPositions.get(i - 1).x,
+                lastPositions.get(i - 1).y,
+                bodySprites.get(i).getWidth(),
+                bodySprites.get(i).getHeight()
+            );
+        }
+    }
+
+    // Check if head overlaps a rectangle
+    public boolean checkHeadOverlap(Rectangle query){
+        return bodyRectangles.get(0).overlaps(query);
+    }
+
+    // Draw the enemy method
+    public void draw(SpriteBatch batch){
+        for(Sprite s : bodySprites){
+            s.draw(batch);
+        }
+    }
+
+    public void dispose(){
+        enemyTexture.dispose();
+    }
+
+
+
+
+}
